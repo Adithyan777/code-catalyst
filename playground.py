eb6a848312cc6e0a84337ffb27edbb5074b7608f
Adithyan K <adithyn.krshna@gmail.com>
2025-01-28 00:47:04 +0530

    Added all prompt templates.

--- playground.py
+++ playground.py
@@ -1,10 +1,10 @@
 from prompts.prompt_manager import PromptManager
 
 def main():
-    # prompt = PromptManager.get_prompt("extract_info_agent", setup="docker")
-    info = PromptManager.get_template_info("extract_info_agent", setup="docker")
-    # print(prompt)
-    print(info.get("description"))
+    prompt = PromptManager.get_prompt("compose_agent", setup="docker")
+    info = PromptManager.get_template_info("compose_agent", setup="docker")
+    print(prompt)
+    print(info)
 
 if __name__ == "__main__":
     main()
